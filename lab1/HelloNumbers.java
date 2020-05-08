89bafdd08ca0746741d8e8a74fd7cadb4d7b3144
guialong <1138073508@qq.com>
2020-05-08 18:05:19 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,13 @@
+public class HelloNumbers{
+   public static void main(String[] args){
+      int x = 0;
+      int y=0;
+      int z=0;
+      while(x<10){
+         System.out.println(z + " ");
+         y=x;
+	 x = x + 1;
+	 z=x+z;
+      }
+   }
+}
