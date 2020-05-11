3cd70a3ca5336e6fb4d24a8a12cec65b18dafd44
guialong <1138073508@qq.com>
2020-05-11 16:27:11 +0800

    proj0-the planet class and its constructor

---
+++ proj0/Planet.java
@@ -0,0 +1,14 @@
+public class Planet {
+	double xxPos = 1.0;
+	double yyPos = 2.0;
+	double xxVel = 3.0;
+	double yyVel = 4.0;
+	double mass = 5.0;
+	String imgFileName = "jupiter.gif";
+	public Planet(double xP , double yP, double xV, double yV, double m, String img) {
+		
+	}
+	public Planet(Planet b) {
+
+	}
+}
