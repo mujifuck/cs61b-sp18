27f4b1e9a0c1974782e750843347f0745fd10ec5
guialong <1138073508@qq.com>
2020-07-11 15:10:13 +0800

    the lab 6 has finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,112 @@
 package hw2;
 
+import edu.princeton.cs.algs4.UF;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int[][] percolation;
+    private int N;
+    private int size;          //count the number of open sites
+    private int OPEN = 1;
+    private int BLOCKED = 0;
+
+    private int top; // the virtual node
+    private int bottom; // the virtual node
+
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF ufTopOnly;
+
+    /** create N-by-N grid, with all sites initially blocked. */
+    public Percolation(int N) {
+        if (N <= 0) throw new IllegalArgumentException("the N must be more than 0.");
+        this.N = N;
+        percolation = new int[N][N];
+        for (int i = 0;i < N;i ++) {
+            for (int j = 0;j< N;j ++) {
+                percolation[i][j] = BLOCKED;
+            }
+        }
+        this.size = 0;
+        this.top = 0; // the virtual top node
+        this.bottom = N * N + 1; // the virtual bottom node
+        uf = new WeightedQuickUnionUF(N * N + 2);
+        ufTopOnly = new WeightedQuickUnionUF(N * N + 1);
+    }
+
+    private int xyTo1D(int r, int c) {
+        return  (r * this.N + c);
+    }
+
+    /** open the site (row, col) if it is not open already. */
+    public void open(int row, int col) {
+        if (row < 0 || row > (N - 1) || col < 0 || col > (N - 1))
+            throw new IndexOutOfBoundsException("the index is out of the percolation.");
+
+        //open the site (row,col)
+        if (percolation[row][col] == OPEN) return;
+        percolation[row][col] = OPEN;
+
+        //check the left,right,up,down is open or blocked, if it is open union them.
+        //顶端必定通
+        if (row == 0) {
+            uf.union(xyTo1D(row, col), top);
+            ufTopOnly.union(xyTo1D(row, col), top);
+        }
+        if (row > 0 && isOpen(row - 1, col)) {
+            uf.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+            ufTopOnly.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+        }
+        if (row < N - 1 && isOpen(row + 1, col)) {
+            uf.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+            ufTopOnly.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+        }
+        if (row == N - 1 && !percolates()) {
+            uf.union(xyTo1D(row, col), bottom);
+        }
+        if (col > 0 && isOpen(row, col - 1)) {
+            uf.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+            ufTopOnly.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+        }
+        if (col < N - 1 && isOpen(row, col + 1)) {
+            uf.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+            ufTopOnly.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+        }
+        size += 1;
+    }
+
+    /** is the site (row, col) open? */
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row > (N - 1) || col < 0 || col > (N - 1))
+            throw new IndexOutOfBoundsException("the index is out of the percolation.");
+        return percolation[row][col] == OPEN;
+    }
+    /** is the site (row, col) full? */
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row > (N - 1) || col < 0 || col > (N - 1))
+            throw new IndexOutOfBoundsException("the index is out of the percolation.");
+        if (!isOpen(row, col))
+            return false;
+        return ufTopOnly.connected(xyTo1D(row, col), top);
+    }
+
+    /** number of open sites. */
+    public int numberOfOpenSites() {
+        return size;
+    }
+
+    /** does the system percolate? */
+    public boolean percolates() {
+        return uf.connected(top, bottom);
+    }
+
+
+    /** use for unit testing (not required). */
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        for(int i = 0;i < a.length;i ++) {
+            a[i] = 0;
+        }
+        System.out.println(a.length);
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,59 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
+
+    private int timeTrial;  // times
+    private double[] fractions;
+    // u = (x1 + x2 + ... + xt) / T
+    // x1 = 204/400
+    /** perform T independent experiments on an N-by-N grid. */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (T <= 0 || N <= 0)
+            throw new IllegalArgumentException("the N or T is out of index.");
+        Percolation p = pf.make(N);
+        this.timeTrial = T;
+        fractions = new double[T];
+        for (int i = 0;i < T;i ++) {
+            while (!p.percolates()) {
+
+                int r = StdRandom.uniform(N);
+                int c = StdRandom.uniform(N);
+                if (!p.isOpen(r, c)) {
+                    p.open(r, c);
+                }
+            }
+            fractions[i] = (double) p.numberOfOpenSites();
+        }
+    }
+
+    /** sample mean of percolation threshold. */
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
+
+    /** sample standard deviation of percolation threshold. */
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
+
+    /** low endpoint of 95% confidence interval. */
+    public double confidenceLow() {
+        double mea = mean();
+        double std = stddev();
+        double sig = 1.96;
+        return  mea - (sig * std) / Math.sqrt(timeTrial);
+    }
+
+    /** high endpoint of 95% confidence interval. */
+    public double confidenceHigh() {
+        double mea = mean();
+        double std = stddev();
+        double sig = 1.96;
+        return  mea + (sig * std) / Math.sqrt(timeTrial);
+    }
+
 
 }
